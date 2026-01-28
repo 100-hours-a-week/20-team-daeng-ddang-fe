95ba5230387cda77acc12e15281e13414eb7b2dd
Dindory <22jysong@gmail.com>
2026-01-28 21:48:57 +0900

    fix: any 타입 수정

--- daengdong/types/global.d.ts
+++ daengdong/types/global.d.ts
@@ -1,9 +1,38 @@
-export { };
+import {
+    NaverLatLng,
+    NaverPoint,
+    NaverMap,
+    NaverMapOptions,
+    NaverMarker,
+    NaverMarkerOptions,
+    NaverPolygon,
+    NaverPolygonOptions,
+    NaverPolyline,
+    NaverPolylineOptions,
+    NaverEventListener
+} from './naver-maps';
 
 declare global {
     interface Window {
-        naver: any;
+        naver: {
+            maps: {
+                LatLng: new (lat: number, lng: number) => NaverLatLng;
+                Point: new (x: number, y: number) => NaverPoint;
+                Map: new (element: string | HTMLElement, options: NaverMapOptions) => NaverMap;
+                Marker: new (options: NaverMarkerOptions) => NaverMarker;
+                Polygon: new (options: NaverPolygonOptions) => NaverPolygon;
+                Polyline: new (options: NaverPolylineOptions) => NaverPolyline;
+                Event: {
+                    addListener: (target: unknown, eventName: string, handler: () => void) => NaverEventListener;
+                    removeListener: (listener: NaverEventListener) => void;
+                };
+                Position: {
+                    TOP_RIGHT: unknown;
+                };
+            };
+        };
         initNaverMap?: () => void;
         getWalkSnapshotBlob?: () => Promise<Blob | null>;
     }
 }
+
