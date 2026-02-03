2e2db94b7b16871326893f5629c30bbd71137629
Dindory <22jysong@gmail.com>
2026-02-04 08:47:36 +0900

    style: font 수정

--- daengdong/app/mypage/page.tsx
+++ daengdong/app/mypage/page.tsx
@@ -113,11 +113,11 @@ export default function MyPage() {
 }
 
 const LoadingOverlay = styled.div`
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.4);
   backdrop-filter: blur(2px);
   display: flex;
