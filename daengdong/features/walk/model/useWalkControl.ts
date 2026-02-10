80793a05b0db0fe8c1c16d62a811592d2468350d
Dindory <22jysong@gmail.com>
2026-02-11 01:28:02 +0900

    fix: 사용되지 않는 변수 제거 (lint 에러 해결)

--- daengdong/features/walk/model/useWalkControl.ts
+++ daengdong/features/walk/model/useWalkControl.ts
@@ -26,7 +26,6 @@ export const useWalkControl = () => {
         startWalk,
         endWalk,
         reset,
-        myBlocks,
         setWalkResult,
         setMyBlocks,
         setOthersBlocks,
