ccb0898a592c1afd1e27a347493e622538e1ddd3
Dindory <22jysong@gmail.com>
2026-02-24 19:39:13 +0900

    fix: 표정분석 결과 컴포넌트 Suspense로 감싸는 로직 추가

--- daengdong/app/walk/expression/result/page.tsx
+++ daengdong/app/walk/expression/result/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Suspense } from "react";
 import ExpressionResultPage from "@/views/walk/expression/result/ExpressionResultPage";
+import { LoadingView } from "@/widgets/GlobalLoading";
 
 export default function Page() {
-  return <ExpressionResultPage />;
+  return (
+    <Suspense fallback={<LoadingView message="결과를 불러오는 중입니다..." />}>
+      <ExpressionResultPage />
+    </Suspense>
+  );
 }
